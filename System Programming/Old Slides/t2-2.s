b99afdcc356d7aa83d75ff3e8c4029dc1a7c176d
Adarsh-sophos <adarshjain583@gmail.com>
2018-01-18 14:19:59 +0530

    4th semester

---
+++ System Programming/Old Slides/t2-2.s
@@ -0,0 +1,39 @@
+	.file	"t2.c"
+	.section	.rodata
+.LC0:
+	.string	"Result is %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	subl	$32, %esp
+
+
+	movl	$10, 20(%esp)
+	movl	$0, 24(%esp)
+	movl	$4, 28(%esp)
+
+
+	movl	$.LC0, %eax
+	movl	28(%esp), %edx
+	movl	%edx, 4(%esp)
+	movl	%eax, (%esp)
+	movl	$0, %eax
+	call	printf
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu/Linaro 4.6.3-1ubuntu5) 4.6.3"
+	.section	.note.GNU-stack,"",@progbits
